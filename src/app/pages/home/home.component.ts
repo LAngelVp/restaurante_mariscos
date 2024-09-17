ec2851287fcd1f92d4f0c47b7747164bfedd0ff5
angelvallejop9610@gmail.com <angelvallejop9610@gmail.com>
2024-09-17 08:00:47 -0600

    cambios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HistoryComponent } from "../../components/history/history.component";
 import { MissionandvisionComponent } from "../../components/missionandvision/missionandvision.component";
 import { SpecialDishesComponent } from "../../components/special-dishes/special-dishes.component";
@@ -9,18 +9,38 @@ import { ImageGaleryComponent } from "../../components/image-galery/image-galery
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HistoryComponent, MissionandvisionComponent, SpecialDishesComponent, BranchesComponent, ComentComponent, ImageGaleryComponent],
+  imports: [
+    HistoryComponent, 
+    MissionandvisionComponent, 
+    SpecialDishesComponent, 
+    BranchesComponent, 
+    ComentComponent, 
+    ImageGaleryComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent implements OnInit {
+  videoMovil: string = '../../../assets/video/video_header_movil.mp4';
+  videoDesktop: string = '../../../assets/video/video_header_desktop.mp4';
   @ViewChild('videoP') videoPlay!: ElementRef<HTMLVideoElement>
 
-  ngAfterViewInit(): void {
-    const video : HTMLVideoElement = this.videoPlay.nativeElement;
+  ngOnInit(): void {
+    const videos: NodeListOf<HTMLVideoElement> = document.querySelectorAll('.video');
 
-    video.autoplay = true;
-    video.loop = true;
-    video.muted = true;
+    videos.forEach(video => {
+      video.autoplay = true;
+      video.loop = true;
+      video.muted = true;
+    });
+  }
+  ngAfterViewInit(): void {
+    const videos: NodeListOf<HTMLVideoElement> = document.querySelectorAll('.video');
+
+    videos.forEach(video => {
+      video.autoplay = true;
+      video.loop = true;
+      video.muted = true;
+    });
   }
 }
