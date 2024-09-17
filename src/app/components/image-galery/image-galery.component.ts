3405776eaa9841086dd93e9b943a327cd1e040d7
angelvallejop9610@gmail.com <angelvallejop9610@gmail.com>
2024-09-17 07:35:55 -0600

    cambios

---
+++ src/app/components/image-galery/image-galery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-galery',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-galery.component.html',
+  styleUrl: './image-galery.component.sass'
+})
+export class ImageGaleryComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { MissionandvisionComponent } from "../../components/missionandvision/mis
 import { SpecialDishesComponent } from "../../components/special-dishes/special-dishes.component";
 import { BranchesComponent } from "../../components/branches/branches.component";
 import { ComentComponent } from "../../components/coment/coment.component";
+import { ImageGaleryComponent } from "../../components/image-galery/image-galery.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HistoryComponent, MissionandvisionComponent, SpecialDishesComponent, BranchesComponent, ComentComponent],
+  imports: [HistoryComponent, MissionandvisionComponent, SpecialDishesComponent, BranchesComponent, ComentComponent, ImageGaleryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
